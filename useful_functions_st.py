c5043fd6f96c436381f118a3ac66b96e0fa408dc
MogwaiA <138663939+MogwaiA@users.noreply.github.com>
2024-08-01 11:37:07 +0200

    Update useful_functions_st.py

--- useful_functions_st.py
+++ useful_functions_st.py
@@ -85,7 +85,13 @@ def download_kmz_file(date_str,proxies):
     else:
         kmz_url = f"{base_url}/{date_str}_wsp64knt120hr_5km.kmz"
         file_path = os.path.join(cache_dir, f"{date_str}.kml")
-            
+    response = requests.get(kmz_url,proxies=proxies,verify=False)
+    response.raise_for_status()
+    kmz_content = BytesIO(response.content)
+    kml_content = dezip_kmz(response)
+    # Stocker le fichier dans le cache de Streamlit
+    with open(file_path, 'wb') as f:
+        f.write(kml_content)
     try:
         response = requests.get(kmz_url,proxies=proxies,verify=False)
         response.raise_for_status()
