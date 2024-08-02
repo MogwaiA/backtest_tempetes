f40636aa75f3f84b30d2bc81e07d4918955b2645
MogwaiA <138663939+MogwaiA@users.noreply.github.com>
2024-08-02 14:20:02 +0200

    Update main_st.py

--- main_st.py
+++ main_st.py
@@ -34,6 +34,18 @@ def main():
         cache_dir="cache"
         if choice=='Vision à une date précise':
             file_path = os.path.join(cache_dir, f"{date_str}.kml")
+             # Convert the KML to GeoDataFrame
+            with open(file_path, "r", encoding="utf-8") as f:
+                kml_data = f.read()
+            gdf = kml_to_geojson(kml_data)
+            
+            # Display the GeoDataFrame
+            st.write("GeoDataFrame:")
+            st.write(gdf)
+
+            # Optional: Display the geometry map
+            st.write("Map:")
+            st.map(gdf)
             try:
                 # Convert the KML to GeoDataFrame
                 with open(file_path, "r", encoding="utf-8") as f:
